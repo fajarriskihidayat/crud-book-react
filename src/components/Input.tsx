3ce615ae4fddd92de3274e48fad641a6c81f91bd
fajarriskihidayat <fajarrh2002@gmail.com>
2025-01-11 05:52:51 +0700

    refactor: className style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,13 @@
 import { FC, InputHTMLAttributes } from "react";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
 
-const Input: FC<InputProps> = ({ ...props }) => {
+const Input: FC<InputProps> = ({ className, ...props }) => {
   return (
     <input
-      className="w-full bg-neutral-100 p-3 rounded-lg focus:outline-black"
+      className={`w-full bg-neutral-100 p-3 rounded-lg focus:outline-black ${className}`}
       {...props}
     />
   );
